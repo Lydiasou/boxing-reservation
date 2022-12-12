c01e7a0a653466f849f3b7d4abd6379c6c0e45d7
Lydiasou <74137307+Lydiasou@users.noreply.github.com>
2022-12-12 11:27:36 +0100

    skip authentification on session controller

--- boxing-reservation/app/controllers/sessions_controller.rb
+++ boxing-reservation/app/controllers/sessions_controller.rb
@@ -1,2 +1,62 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_session, only: [:show, :edit, :destroy]
+
+  def index
+    @session = Session.all
+  end
+
+  def show
+  end
+
+  def new
+    @session = Session.new
+    authorize @session
+  end
+
+  def create
+    @session = Session.new(session_params)
+    @session.user = current_user
+    # authorize @session
+    if @session.save
+      redirect_to session_path(@session)
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @session.update(session_params)
+    redirect_to session_path(@session)
+  end
+
+  def destroy
+    # authorize @session
+    @session.destroy
+    redirect_to session_path, status: :see_other
+  end
+
+  private
+
+  def set_session
+    @session = Session.find(params[:id])
+  end
+
+  def session_params
+    params.require(:session).permit(
+      :title,
+      :date,
+      :start_at,
+      :end_at,
+      :description,
+      :coach,
+      :address,
+      :title_address,
+      :price,
+      :max_participation
+    )
+  end
 end
